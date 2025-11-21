99d35ef4a929e55fba70a7d829ea80d809bbeb74
yunus_kongrad <167466650+YunusKongrad@users.noreply.github.com>
2025-11-22 02:35:41 +0300

    power up nitro
    bug vardi duzeltildi eski degerine donmuyordu ayarladim

--- Assets/Scripts/PU_Scripts/PU_Nitro.cs
+++ Assets/Scripts/PU_Scripts/PU_Nitro.cs
@@ -4,47 +4,31 @@ using System.Security.Cryptography;
 
 public class PU_Nitro : MonoBehaviour
 {
-    /*
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player") || other.CompareTag("Player2"))
-        {
-            CarPowerUpHandler carHandler = other.GetComponent<CarPowerUpHandler>();
-            if (carHandler != null)
-            {
-                // Arabanın "cüzdanına" Nitro güçlendirmesini ekle
-                carHandler.GivePowerUp("Nitro");
-                // Ve kendini yok et
-                Destroy(gameObject);
-            }
-        }
-    }
-    */
     public float duration = 10f;
     public IEnumerator Nitro(MonoBehaviour target)
     {
-        float originalNitro = 0f;
+        float originalNitroRate = 0f; 
 
         if (target is DriveMyCar p1)
         {
-            originalNitro = p1.nitroRechargeRate;
-            p1.nitroRechargeRate *= (1.5f);
+            originalNitroRate = p1.nitroRechargeRate;
+            p1.nitroRechargeRate *= 1.5f; 
         }
         else if (target is DriveMyCar_Player2 p2)
         {
-            originalNitro = p2.nitroRechargeRate;
-            p2.nitroRechargeRate *= (1.5f);
+            originalNitroRate = p2.nitroRechargeRate;
+            p2.nitroRechargeRate *= 1.5f;
         }
 
         yield return new WaitForSeconds(duration);
 
-        if (target is DriveMyCar_Player2 p1r)
+        if (target is DriveMyCar p1_after) 
         {
-            p1r.speed = originalNitro;
+            p1_after.nitroRechargeRate = originalNitroRate; 
         }
-        else if (target is DriveMyCar_Player2 p2r)
+        else if (target is DriveMyCar_Player2 p2_after)
         {
-            p2r.speed = originalNitro;
+            p2_after.nitroRechargeRate = originalNitroRate;
         }
     }
 }
