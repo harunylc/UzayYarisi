6fdd5bb1a2515b04cdd415c8a663808f2bdd5c00
Harun <hrn.ylc.7007@gmail.com>
2025-11-24 01:20:54 +0300

    İlk dengeleme testi

--- Assets/Harun_Upgrade/Scripts/CarStatsData.cs
+++ Assets/Harun_Upgrade/Scripts/CarStatsData.cs
@@ -6,11 +6,11 @@ public class CarStatsData : ScriptableObject
     public int carIndex; 
     
     // Değerleri değiştirmek istersen buradaki sayıları değiştireceksin.
-    public const float MinHiz = 4000f;     public const float MaxHiz = 12000f;
-    public const float MinFren = 10f;      public const float MaxFren = 60f; // Kod bunu 20'ye bölüp Drag yapacak
-    public const float MinNitro = 2000f;   public const float MaxNitro = 8000f;
-    public const float MinYol = 1500f;     public const float MaxYol = 6000f;
-    public const float MinAgir = 800f;     public const float MaxAgir = 2200f;
+    public const float MinHiz = 6000f;     public const float MaxHiz = 14000f; 
+    public const float MinFren = 20f;      public const float MaxFren = 70f;
+    public const float MinNitro = 3000f;   public const float MaxNitro = 9000f;
+    public const float MinYol = 2000f;     public const float MaxYol = 7000f;
+    public const float MinAgir = 1000f;    public const float MaxAgir = 2500f;
     
     [Header("Temel Özellikler")]
     [Tooltip("Aracın başlangıç hızı. Min-Max değerleri koddan ayarlanır.")]
